e64ad4f0054210a718d4eba7e6bbed5e01d04b98
Anurag Srigyan <hiimanurag122@gmail.com>
2024-04-28 02:53:51 +0530

    add items spinner

--- src/components/automatedTasks/TaskTable.jsx
+++ src/components/automatedTasks/TaskTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, TableBody, Fab, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+import { Table, TableBody, Fab, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
 import TableRowItemWithDelete from './TableRowItem';
